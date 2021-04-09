d53cecb9c99a321d126031f4ded8b0d03f2ab743
Alexander Vazhenin <a.vazhenin413@gmail.com>
2021-04-09 19:01:46 +0500

    Updated variables in updateMagazine

--- backend/src/lambda/http/updateMagazine.ts
+++ backend/src/lambda/http/updateMagazine.ts
@@ -2,21 +2,21 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
-import { UpdateTodoRequest } from '../../requests/UpdateMagazineRequest'
-import { updateTodo } from '../../userActions/magazines'
+import { UpdateMagazineRequest } from '../../requests/UpdateMagazineRequest'
+import { updateMagazine } from '../../userActions/magazines'
 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  const magazineId = event.pathParameters.magazineId
+  const updatedMagazine: UpdateMagazineRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  // TODO: Update a magazine item with the provided id using values in the "updatedMagazine" object
 
   const authorization = event.headers.Authorization
   const split = authorization.split(' ')
   const jwtToken = split[1]
 
-  await updateTodo(updatedTodo, todoId, jwtToken)
+  await updateMagazine(updatedMagazine, magazineId, jwtToken)
 
   return {
     statusCode: 201,
