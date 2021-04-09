bdb1fdbbfd2ceea013539a3426576966e91be7eb
Alexander Vazhenin <a.vazhenin413@gmail.com>
2021-04-09 18:38:11 +0500

    Updated functions in getMagazines

--- backend/src/lambda/http/getMagazines.ts
+++ backend/src/lambda/http/getMagazines.ts
@@ -1,16 +1,16 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import {getAllTodos} from '../../userActions/magazines'
+import { getAllMagazines } from '../../userActions/magazines'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  // TODO: Get all magazine items for a current user
 
   const authorization = event.headers.Authorization
   const split = authorization.split(' ')
   const jwtToken = split[1]
 
-  const todos = await getAllTodos(jwtToken)
+  const magazines = await getAllMagazines(jwtToken)
 
   return {
     statusCode: 200,
@@ -19,7 +19,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
-      items: todos
+      items: magazines
     })
   }
 }
